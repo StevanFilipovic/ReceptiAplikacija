a3d812a70b3575f56d4ddc52b66eb8cb0e67460b
stevan.filipovic <stevan.filipovic@proudsourceit.rs>
2017-11-14 15:02:29 +0100

    Drugi komit

---
+++ src/app/shopping-list/shoping-edit/shoping-edit.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-shoping-edit',
+  templateUrl: './shoping-edit.component.html',
+  styleUrls: ['./shoping-edit.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ShopingEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
